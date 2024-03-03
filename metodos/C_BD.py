d29cdd22532f5d42eea9388eadafad95b0dc4d87
franchezco456 <frankyfranco456@gmail.com>
2024-03-03 15:58:14 -0500

    WARD

--- metodos/C_BD.py
+++ metodos/C_BD.py
@@ -1,12 +1,15 @@
 #intalar pymysql
 import pymysql
+#conexion a la base de datos
 conexionUsuarios= pymysql.connect(host='localhost', user = 'root', passwd='root', db='usuarios')
+#se crea un cur con la instancia de la conexion para hacer consultas
 cur=conexionUsuarios.cursor()
-
+#buscamos en las columnas id y password de la tabla users_passwords
 cur.execute("select ID, PASSWORD from users_passwords")
+#imprimimos los datos 
 for ID, PASSWORD in cur.fetchall():
     print(ID , " | " , PASSWORD)
-   
+#se cierra la conexion a la base de datos
 conexionUsuarios.close()
 
 
