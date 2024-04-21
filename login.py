5315d4dc32ea67d8ac0bfb208305f638ff4b6bd6
franchezco456 <frankyfranco456@gmail.com>
2024-04-21 15:38:13 -0500

    registrarse

--- login.py
+++ login.py
@@ -1,7 +1,7 @@
 from customtkinter import CTk, CTkFrame, CTkEntry, CTkButton, CTkLabel
 from tkinter import Tk, PhotoImage
 
-from metodos.database import verificacion_existe
+from metodos.database import verificacion_existe, crear_Usuario
 #Variables:
 c_negro = '#010101'
 c_azul  = '#3B83BD'
@@ -13,6 +13,13 @@ def credenciales_usuario():
      user_password=contraseña.get()
      print(user_password)
      verificacion_existe(user_id,user_password)
+
+def new_user():
+     user_id=int(usuario.get())
+     print(user_id)
+     user_password=contraseña.get()
+     print(user_password)
+     crear_Usuario(user_id,user_password)
 
 
 
@@ -53,7 +60,7 @@ contraseña = CTkEntry(ventana, placeholder_text = 'Contraseña',
 contraseña.grid(columnspan =2, row=2, padx=4, pady=4)
 
 
-#Boton recordarme
+
 
 
 #Boton iniciar sesion
@@ -63,7 +70,7 @@ bt_iniciar.grid(columnspan=1, row= 4, pady=4, padx=4)
 
 #Boton registrarse
 bt_Registrar= CTkButton(ventana, border_color=c_azul, fg_color= c_negro, 
-                      hover_color=c_morado, corner_radius=12, border_width=2 ,text='Registrarse', height=40)
+                      hover_color=c_morado, corner_radius=12, border_width=2 ,text='Registrarse', height=40,command=new_user)
 bt_Registrar.grid(columnspan=2, row= 5, pady=4, padx=4)
 
 
